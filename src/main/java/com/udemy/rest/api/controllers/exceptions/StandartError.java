3408912aab5fc062b4d02ede2dcc69b7d219bd94
joaopzta <joaopzta@gmail.com>
2022-03-13 17:30:23 -0300

    [bug-fix] - Add default Constructor

--- src/main/java/com/udemy/rest/api/controllers/exceptions/StandartError.java
+++ src/main/java/com/udemy/rest/api/controllers/exceptions/StandartError.java
@@ -3,11 +3,13 @@ package com.udemy.rest.api.controllers.exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class StandartError {
 
--- src/main/java/com/udemy/rest/api/model/User.java
+++ src/main/java/com/udemy/rest/api/model/User.java
@@ -3,12 +3,14 @@ package com.udemy.rest.api.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "TB_USER")
 @Builder
 public class User {
